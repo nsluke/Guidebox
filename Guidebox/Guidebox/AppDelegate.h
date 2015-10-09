7cd2ba99ad91b907019afdb6c5654c96a1d7ab5a
Luke Solomon <luke@makeschool.com>
2015-10-08 20:47:10 -0700

    initial commit

---
+++ Guidebox/Guidebox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Guidebox
+//
+//  Created by Luke Solomon on 10/8/15.
+//  Copyright © 2015 Luke Solomon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Guidebox/Guidebox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Guidebox
+//
+//  Created by Luke Solomon on 10/8/15.
+//  Copyright © 2015 Luke Solomon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
